757f733297c9e769c10769b1221352f6a4839955
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2023-08-29 17:36:45 -0300

    [refactor][style] atualiza a estilização dos formulários.

--- src/components/TelaCadastro/styled.js
+++ src/components/TelaCadastro/styled.js
@@ -15,43 +15,46 @@ export const FormContainer = styled.div`
 export const Form = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 2rem;
+  width: 400px;
+  gap: 5px;
 `;
 
 export const Input = styled.input`
   padding: 8px;
-  margin-left: 16px;
+  width: 100%;
+  grid-column: 1/3;
+  border-radius: 5px;
+  border: 1px solid gray;
 `;
 
 export const SendButton = styled.button`
   background-color: #ff9421;
   border-radius: 8px;
   padding: 16px;
-  width: 200px;
+  width: 300px;
   border: none;
   color: white;
   margin-top: 32px;
   cursor: pointer;
   font-weight: bold;
+  align-self: center;
 `;
 
 export const BackToLoginButton = styled.button`
   background-color: white;
   border-radius: 8px;
   cursor: pointer;
-  width: 200px;
+  width: 300px;
   padding: 10px;
   :hover {
     background-color: black;
     color: white;
   }
+  align-self: center;
+  margin-top: 15px;
 `;
 
 export const StyledLabel = styled.label`
-  width: 300px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  margin-top: 15px;
+  color: gray;
 `;
